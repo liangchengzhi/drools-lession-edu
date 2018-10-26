5f02f33e31674a24a9f947acff93b1c7d5dd7b2d
liangcz <492312425@qq.com>
2018-10-26 10:06:21 +0800

    drools lession

---
+++ xjc-drools-lession/src/main/java/com/xjc/drools/lession/App.java
@@ -0,0 +1,13 @@
+package com.xjc.drools.lession;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
